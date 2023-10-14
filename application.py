3383c106e1d4e23218f8a6a539852f30c11e02cf
kousik sasmal <kousik712417@gmail.com>
2023-10-14 21:51:31 +0530

    changes for AWS EB

--- application.py
+++ application.py
@@ -9,8 +9,8 @@ webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
-
+application = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+app = application
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -35,4 +35,5 @@ def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
